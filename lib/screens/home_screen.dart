c383e5d84c3b81c211574dce06e5d1aaec2aecb4
stefannyToror <stefannyrami@gmail.com>
2026-06-04 09:34:35 -0500

    feat(ui): improve home screen app bar styling

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-	const HomeScreen({super.key});
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(centerTitle: false, title: Text("List of Widgets")),
-      body: Center(),
+      appBar: AppBar(
+        centerTitle: false,
+        toolbarHeight: 72,
+        elevation: 0,
+        title: const Text(
+          'List of Widgets',
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.w800),
+        ),
+      ),
+      body: const Center(),
     );
   }
 }
